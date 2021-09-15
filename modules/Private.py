f4248e3d1774800251be91de63128df446eeffa4
MR.DEVOLOPER <89473448+MR-DEVELOPER-2003@users.noreply.github.com>
2021-09-15 17:49:23 +0530

    Update Private.py

--- modules/Private.py
+++ modules/Private.py
@@ -1,71 +1,49 @@
-
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import Client, filters
-import signal
-import os
-import sys
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 
-HOME_TEXT = "<b>Helo, [{}](tg://user?id={})\n\n• Iam A Bot Project made by @Sadlife\n# my group @quicktrivia\n• I Can Manage Group VC's\n\n• Hit /help to know about available commands.</b>"
-HELP = """
-🎧 <b>I Can Play Musics On VoiceChats 🤪</b>
-
-🎶 **Common Commands**:by @Sadlife56 or aditya
-• `/song` __Download Song from youtube__
-• `/play`  __Play song you requested__
-• `/help` __Show help for commands__
-• `/dplay` __Play song you requested via deezer__
-• `splay` __Play song you requested via jio saavn__
-• `/ytplay` __Play song directly from youtube server__
-• `/search` __Search video songs links__
-• `/current` __Show now playing__
-• `/playlist` __Show now playing list__
-• `/video` __Downloads video song quickly__
-🎶 **Admin Commands**:*aditya, tushar, lucky, harsh, utkarsh, sg*
-• `/player`  __Open music player settings panel__
-• `/pause` __Pause song play__
-• `/skip` __Skip next song__
-• `/resume`  __Resume song play__
-• `/userbotjoin`  __Invites assistant to your chat__
-• `/end` __Stops music play__
-• `/admincache` __Refresh list of admins with vc power__
-© Powered By 
-[ __@quicltrivia || @Sadlife56__ ]
-"""
+from helpers.filters import other_filters2
 
 
+@Client.on_message(other_filters2)
+async def start(_, message: Message):
+    await message.reply_photo("https://telegra.ph/file/557996394db2a9781f2b2.jpg")
+    await message.reply_text(
+        f"""**Hey, I'm 𓄂AǫᴜᴀMᴀɴ ࿐ | Mᴜsɪᴄ Bᴏᴛ |🎵
 
-@Client.on_message(filters.command('start'))
-async def start(client, message):
-    buttons = [
-       [
-                InlineKeyboardButton('📢 Updates', url='https://t.me/v4updates'),
-                InlineKeyboardButton('💬 Support', url='https://t.me/v4updatesdiscussion')
-                ],[
-                InlineKeyboardButton('🤖 Developer', url='https://t.me/aboutmeaditya'),
-                InlineKeyboardButton('🎧 Chats', url='https://t.me/quicktrivia')
-                ],[
-                InlineKeyboardButton('📜 Source Code 📜', url='https://telegra.ph/file/776826cc26ba5897a9ec2.mp4'),
-    ]
-    ]
-    reply_markup = InlineKeyboardMarkup(buttons)
-    await message.reply_photo(photo="https://telegra.ph/file/cd1c80751b6c75a655b1d.jpg", caption=HOME_TEXT.format(message.from_user.first_name, message.from_user.id), reply_markup=reply_markup)
-    await message.delete()
+I can play ꬺᶙȿᶖɕ  in your group's voice CHAT Developed by [𓄂AǫᴜᴀMᴀɴ ࿐](https://t.me/AQUAMAN_XD)
+
+Add me to your group and play music freely😆!**
+        """,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Oᴡɴᴇʀ", url="https://t.me/AQUAMAN_XD")
+                  ],[
+                    InlineKeyboardButton(
+                        "🛡 SUPPORT GROUP 🛡", url="https://t.me/AQUAMAN_XD_WORLD"
+                    ),
+                ],[ 
+                    InlineKeyboardButton(
+                        "GROUP ME LEJAO 😆", url="https://t.me/AQUAMAN_MUSIC_BOT?startgroup=true"
+                    )]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
+
+@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
+async def gstart(_, message: Message):
+      await message.reply_text("""**BOT IS WORKING**""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🛡 SUPPORT 🛡", url="https://t.me/AQUAMAN_XD_WORLD")
+                ]
+            ]
+        )
+   )
 
 
-@Client.on_message(filters.command("help"))
-async def show_help(client, message):
-    buttons = [
-        [
-                InlineKeyboardButton('📢 Updates', url='https://t.me/v4updates'),
-                InlineKeyboardButton('💬 Support', url='https://t.me/v4updatesdiscussion')
-                ],[
-                InlineKeyboardButton('🤖 Developer', url='https://t.me/aboutmeaditya'),
-                InlineKeyboardButton('🎧 Chats', url='https://t.me/quicktrivia')
-                ],[
-                InlineKeyboardButton('📜 Source Code 📜', url='https://telegra.ph/file/776826cc26ba5897a9ec2.mp4'),
-       ]
-    ]
-    reply_markup = InlineKeyboardMarkup(buttons)
-    await message.reply_photo(photo="https://telegra.ph/file/cd1c80751b6c75a655b1d.jpg", caption=HELP, reply_markup=reply_markup)
-    await message.delete()
